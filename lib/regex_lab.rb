47b80f1bb185764afe9d53b6c5546c11ae352159
Bobby Neal <rsneal01@gmail.com>
2020-04-19 17:12:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z][0-9.!?,;:]$/)
+  if text.scan(/^[A-Z][0-9.!?,;:]$/)
+    return true
+else
+  false
 end
   
 
