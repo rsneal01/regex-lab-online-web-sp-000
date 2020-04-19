7c64b997de0534c1f85da65cfd11dca34f3654ff
Bobby Neal <rsneal01@gmail.com>
2020-04-19 16:14:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,4 +12,15 @@ text.scan(/$(ing)/)
 
 end
 
-/^[aeiouAEIOU]/
+# def words_five_letters_long(text)
+# text.grep(/\w{5}\s/)
+# end
+
+# def first_word_capitalized_and_ends_with_punctuation?(text)
+# text.match(/$())
+# end
+
+# def valid_phone_number?(phone)
+#   scan.phone(/)
+
+# end
