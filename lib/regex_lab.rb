9059efb98eb0a0ce492752df5ebbbfe62177bf43
Bobby Neal <rsneal01@gmail.com>
2020-04-19 17:44:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,8 +21,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
   
 def valid_phone_number?(phone)
-  if phone.match(/[0-9]/) 
-    return true
+  phone.match(/[0-9]/)  ? true : false
   
 end
 
