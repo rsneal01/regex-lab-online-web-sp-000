62436b0524359a2a47adac7ee1c926caca5b944f
Bobby Neal <rsneal01@gmail.com>
2020-04-19 13:32:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,19 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/^[aeiouAEIOU]/)
+  true
+else
+  FALSE
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+text.scan(/^(un)/)
+text.scan(/$(ing)/)
 
 end
 
 def words_five_letters_long(text)
-
+word.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
